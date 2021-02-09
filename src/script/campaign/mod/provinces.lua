474e66b7200c956210a9b8919930b9297d45976a
Mickael Daniel <daniel.mickael@gmail.com>
2021-02-09 01:43:30 +0100

    Disable for troy_amazons_trj_penthesilea

--- src/script/campaign/mod/provinces.lua
+++ src/script/campaign/mod/provinces.lua
@@ -7,9 +7,25 @@ local debug = require("tw-debug")("mk:provinces")
 local getProvincesData = require("provinces/data").getProvincesData
 local ui = require("provinces/ui")
 
+-- list of factions for which this mod is not enabled
+local IGNORED_FACTIONS = { "troy_amazons_trj_penthesilea" }
+
 local function init()
-    debug("Init")
-    ui.build()
+    local faction = cm:get_local_faction()
+    local enabled = true
+
+    for k, v in pairs(IGNORED_FACTIONS) do
+        debug("Check %s against %s", faction, v)
+        if faction == v then
+            debug("Disable mod")
+            enabled = false
+            break
+        end
+    end
+
+    if enabled then
+        ui.build()
+    end
 end
 
 cm:add_first_tick_callback(init)
